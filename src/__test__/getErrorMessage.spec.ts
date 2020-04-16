67bcc7a87a9934309ff6bd1bffce88b7c30e2954
何彭 <rainke@magicare.me>
2020-04-16 23:14:15 +0800

    test: pass

--- src/__test__/getErrorMessage.spec.ts
+++ src/__test__/getErrorMessage.spec.ts
@@ -1,14 +1,84 @@
 import getErrorMessage from '../index';
 
 describe('getErrorMessage', () => {
-  it('should return', () => {
-    expect(getErrorMessage()).toEqual({
+  it('sample error matched', () => {
+    const fixtureStack = `TypeError: Error raised
+      at bar http://192.168.31.8:8000/c.js:2:9`;
+    expect(getErrorMessage(fixtureStack)).toEqual({
       message: 'Error raised',
       stack: [
         {
           line: 2,
           column: 9,
           filename: 'http://192.168.31.8:8000/c.js'
+        }
+      ]
+    });
+  });
+  it('chrome error matched', () => {
+    const fixtureStack = `TypeError: Error raised
+      at bar http://192.168.31.8:8000/c.js:2:9
+      at foo http://192.168.31.8:8000/b.js:4:15
+      at calc http://192.168.31.8:8000/a.js:4:3
+      at <anonymous>:1:11
+      at http://192.168.31.8:8000/a.js:22:3
+    `;
+    expect(getErrorMessage(fixtureStack)).toEqual({
+      message: 'Error raised',
+      stack: [
+        {
+          line: 2,
+          column: 9,
+          filename: 'http://192.168.31.8:8000/c.js'
+        },
+        {
+          line: 4,
+          column: 15,
+          filename: 'http://192.168.31.8:8000/b.js'
+        },
+        {
+          line: 4,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js'
+        },
+        {
+          line: 22,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js'
+        }
+      ]
+    });
+  });
+  it('Firefox error matched', () => {
+    const fixtureFirefoxStack = `
+      bar@http://192.168.31.8:8000/c.js:2:9
+      foo@http://192.168.31.8:8000/b.js:4:15
+      calc@http://192.168.31.8:8000/a.js:4:3
+      <anonymous>:1:11
+      http://192.168.31.8:8000/a.js:22:3
+    `;
+    expect(getErrorMessage(fixtureFirefoxStack)).toEqual({
+      message: '',
+      stack: [
+        {
+          line: 2,
+          column: 9,
+          filename: 'http://192.168.31.8:8000/c.js'
+        },
+        {
+          line: 4,
+          column: 15,
+          filename: 'http://192.168.31.8:8000/b.js'
+        },
+        {
+          line: 4,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js'
+        },
+        {
+          line: 22,
+          column: 3,
+          filename: 'http://192.168.31.8:8000/a.js'
         }
       ]
     });
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
 interface ErrorMessage {
   message: string;
-  stack: Array<{
-    line: number;
-    column: number;
-    filename: string;
-  }>;
+  stack: StackItem[];
 }
 
-function getErrorMessage(): ErrorMessage {
+interface StackItem {
+  line: number;
+  column: number;
+  filename: string;
+}
+
+function getErrorMessage(fixtureStack: string): ErrorMessage {
+  const errors = fixtureStack
+    .split('\n')
+    .map((s) => s.trim())
+    .filter(Boolean);
+  let message = '';
+  let stack: StackItem[] = [];
+  const firstError = errors[0];
+  if (firstError.includes('Error')) {
+    message = firstError.split(':')[1].trim();
+    errors.shift();
+  }
+
+  errors.forEach((error) => {
+    const re = /(http.*):(\d+):(\d+)$/;
+    const match = error.match(re);
+    if (match) {
+      stack.push({ line: Number(match[2]), column: Number(match[3]), filename: match[1] });
+    }
+  });
+
   return {
-    message: 'Error raised',
-    stack: [
-      {
-        line: 2,
-        column: 9,
-        filename: 'http://192.168.31.8:8000/c.js'
-      }
-    ]
+    message,
+    stack
   };
 }
 
